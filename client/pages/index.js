3b807a72e587c21d03445bfa2c167a32a9167490
Kush Patel <kushpatel@Kushs-MacBook-Pro.local>
2022-02-24 08:38:46 -0700

    creating context

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Main from '../components/Main'
 
 const style = {
   wrapper: `h-screen max-h h-min-screen w-screen bg-[#2D242F] text-white select-none flex flex-col justify-between`,
@@ -8,7 +9,7 @@ const style = {
 export default function Home() {
   return <div className={ style.wrapper }>
   <Header />
-    <h2>hello</h2>
+  <Main />
   <h2>hello</h2>
 
   </div>
