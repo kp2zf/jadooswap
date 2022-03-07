d2aff00c8048d9fc4d3f517720279ad18cae904b
Kush Patel <kushpatel@kushs-mbp.lan>
2022-03-07 14:33:29 -0500

    making progress

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Image from 'next/image'
 import {   FiArrowUpRight  } from 'react-icons/fi'
 import {   AiOutlineDown  } from 'react-icons/ai'
 import {   HiOutlineDotsVertical  } from 'react-icons/hi'
 import ethLogo from '../assets/eth.png'
 import uniswaplogo from '../assets/uniswap.png'
+import  { TransactionContext } from '../context/TransactionContext'
 
 const style = {
     wrapper: `p-4 w-screen flex justify-between items-center`,
@@ -26,6 +27,8 @@ const style = {
 
 const Header = () => {
    const [selectedNav, setSelectedNav] = useState('swap')
+   const {connectWallet, currentAccount} = useContext(TransactionContext)
+   console.log({connectWallet, currentAccount})
 
    return  (
     <div className={style.wrapper}>
